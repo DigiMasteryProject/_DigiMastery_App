cda5ceade2531e5601de1666a9b2d7c3952a4196
DigiMasteryProject <digimstrypct@gmail.com>
2026-05-15 22:22:25 +0200

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const rateLimit = require("express-rate-limit");
 const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
