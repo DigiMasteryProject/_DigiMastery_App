966a9ef6056f3ebff79cb9e724cff2db93cfe5bc
DigiMasteryProject <digimstrypct@gmail.com>
2026-05-17 14:52:37 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -95,7 +95,10 @@ app.use((req, res, next) => {
   console.log(`➡️ ${req.method} ${req.url}`);
   next();
 });
-
+app.use((req, res, next) => {
+  console.log("RATE CHECK:", req.method, req.path, req.ip);
+  next();
+});
 /* =========================
    MODELS
 ========================= */
