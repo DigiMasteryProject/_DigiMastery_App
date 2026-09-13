cacaa2b533f5b32f19a53b5a158d7716c5de752b
DigiMasteryProject <digimstrypct@gmail.com>
2026-09-13 12:34:19 +0200

    Update userCampaignRoute.js

--- backend/routes/userCampaignRoute.js
+++ backend/routes/userCampaignRoute.js
@@ -8,7 +8,7 @@ const campaignAccess = require("../middleware/campaignAccessMiddleware");
 
 // GET ALL → SOLO auth
 router.get("/", auth, userCampaignController.getAll);
-router.get("/:id", auth, campaignAccess, userCampaignController.getUserCampaignById);
+router.get("/:id", auth, userCampaignController.getUserCampaignById);
 // CREATE → auth + campaignAccess
 router.post("/", auth, campaignAccess, userCampaignController.createUserCampaign);
 router.put("/:id", auth, campaignAccess, userCampaignController.updateUserCampaign);
